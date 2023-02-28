02ba47d47ec0e75af3def1036b49f824be11b866
Rodrigo Harder <rodrigo.harder@alura.com.br>
2023-02-28 14:06:19 -0300

    Adicionando desafio 1

---
+++ desafio-1-operadores-booleanos/main.js
@@ -0,0 +1,29 @@
+
+
+let numeroUm = 1;
+let stringUm = "1";
+let numeroTrinta = 30;
+let stringTrinta = "30";
+let numeroDez = 10;
+let stringDez = "10";
+
+//Operador de igualdade (==): retorna 'true' se os operadores forem iguais, ou seja, tiverem o mesmo valor
+if(numeroUm == stringUm){
+    console.log('As variáveis numeroUm e stringUm apresentam o mesmo valor, mas tipos diferentes');
+} else{
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+if(numeroTrinta == stringTrinta){
+    console.log('As variáveis numeroTrinta e stringTrinta apresentam o mesmo valor, mas tipos diferentes');
+} else{
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor');
+}
+
+if(numeroDez == stringDez){
+    console.log('As variáveis numeroDez e stringDez apresentam o mesmo valor, mas tipos diferentes');
+} else{
+    console.log('As variáveis numeroTrinta e stringTrinta não apresentam o mesmo valor');
+}
+
+
